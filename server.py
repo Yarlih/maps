cb14d229209594f258ad07d2f4cc11cb63641864
Kateryna Lunina <katlunina1220@gmail.com>
2018-02-12 23:14:33 +0100

    sorted list of countries

--- server.py
+++ server.py
@@ -6,17 +6,13 @@ app = Flask(__name__)
 def main():
     return render_template('index.html', text="Kate")
 
-@app.route("/max")
-def render_max():
-    return render_template('index.html', text="Max")
-
-
 @app.route("/list_of_countries")
 def list_of_countries():
     from map_drawer import list_of_countries_tree
 
-    country = list_of_countries_tree("world.svg")
-    return render_template("list_of_countries.html",countries = country)
+    countries = sorted(list_of_countries_tree("world.svg"), key=lambda x: x[0])
+
+    return render_template("list_of_countries.html",countries = countries)
 
 from map_drawer import draw_country
 import xml.etree.ElementTree as ET
@@ -34,14 +30,8 @@ def colored_countries():
     draw_country(root,ids)
     ET.register_namespace("","http://www.w3.org/2000/svg")
     result =  ET.tostring(root).decode()
-    print(result)
     return render_template("colored_countries.html", svg_string = result)
 
-
-
-
-# print(draw_country(root, ["NL","BE","CH", "CZ", "GR","IT", "TR","UA", "FR", "ES", "RU"]))
-# tree.write('out.svg')
 
 if __name__ == "__main__":
     app.run(port=8080)
